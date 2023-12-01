d4ebba7172d5a0108df181b8cae8a48916e4b07d
Hong-Dae-Gi <hsi1150@gmail.com>
2023-12-01 15:12:51 +0900

    index.js 함수 추가 및 기능 확장

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
     index.html을 위한 js 파일
 */
 
-function add(a,b) {
+function add(a,b,c) {
+    if(c!=undefined) return a+b+c;
     return a+b;
+}
+
+function substract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) { 
+    return a/b;
 }
